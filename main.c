c4599d43e190e96ebd53303646aba5d4441b521e
Farrukh <679@holbertonschool.com>
2019-04-05 21:31:58 +0000

    Added tokarr func

--- main.c
+++ main.c
@@ -5,6 +5,61 @@
 #include <string.h>
 #include <unistd.h>
 
+
+/**
+ * d_space - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: returns the differences of ASCII characters
+ */
+
+char *d_space(char *str)
+{
+	size_t i = 0;
+	int letter = 0;
+
+	while (str && str[i])
+	{
+		if (str[i] == ' ' && letter == 0)
+		{
+			++str;
+			continue;
+		}
+		if (str[i] == ' ' && letter == 1)
+		{
+			str[i] = '\0';
+		}
+		++i;
+		letter = 1;
+	}
+	return (str);
+}
+
+/**
+ * tokarr - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: returns the differences of ASCII characters
+ */
+
+void tokarr(char *str)
+{
+	char *token;
+	char **arr;
+	unsigned int i = 0, count = 1;
+	char *pure = d_space(str);
+
+	while (pure && pure[i])
+	{
+		if (pure[i] == ' ')
+			count++;
+		i++;
+	}
+	printf("%d", count);
+}
+
 /**
  * _strtok - compares two strings
  * @s1: first string
@@ -35,78 +90,50 @@ char *_strtok(char *str, const char *delim)
 	return (p);
 }
 
-/**
- * _strtok - compares two strings
- * @s1: first string
- * @s2: second string
- *
- * Return: returns the differences of ASCII characters
- */
-
-char *d_space(char *str)
-{
-	size_t i = 0;
-	int letter = 0;
-
-	while (str && str[i])
-	{
-		if (str[i] == ' ' && letter == 0)
-		{
-			++str;
-			continue;
-		}
-		if (str[i] == ' ' && letter == 1)
-		{
-			str[i] = '\0';
-		}
-		++i;
-		letter = 1;
-	}
-	return (str);
-}
-
 int main(int argc, char *argv[], char *envp[])
 {
 	char *buffer, *pure;
 	size_t len = 0;
 	int get, pid;
 	char *argv1[] = {"", NULL};
-	while(18)
-	{
-		printf("($) ");
-		get = getline(&buffer, &len, stdin);
-		buffer[get - 1] = '\0';
-		pure = d_space(buffer);
-		//	printf("%s", pure);
-		if (!strcmp(pure, "exit"))
-		{
-			exit(98);
-		}
-		if (get == -1)
-		{
-			printf("\n");
-			break;
-		}
-		pid = fork();
-		if (pid < 0)
-		{
-			printf("Continue");
-			continue;
-		}
-		if (pid == 0)
-		{
-			if (execve(pure, argv1, NULL) == -1)
-			{
-				perror("Error:");
-				return (-1);
-			}
-		}
-		else
-		{
-			wait(NULL);
-			continue;
-		}
-	}
-	free(buffer);
+	char str[] = "  Hello World Hi";
+	tokarr(str);
+	/*while(18)
+		  {
+		  printf("($) ");
+		  get = getline(&buffer, &len, stdin);
+		  buffer[get - 1] = '\0';
+		  pure = d_space(buffer);
+		  //	printf("%s", pure);
+		  if (!strcmp(pure, "exit"))
+		  {
+		  exit(98);
+		  }
+		  if (get == -1)
+		  {
+		  printf("\n");
+		  break;
+		  }
+		  pid = fork();
+		  if (pid < 0)
+		  {
+		  printf("Continue");
+		  continue;
+		  }
+		  if (pid == 0)
+		  {
+		  if (execve(pure, argv1, NULL) == -1)
+		  {
+		  perror("Error:");
+		  return (-1);
+		  }
+		  }
+		  else
+		  {
+		  wait(NULL);
+		  continue;
+		  }
+		  }
+		  free(buffer);*/
 	return (0);
 }
