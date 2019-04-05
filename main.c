5fe61e6365b07e1feb7aa342954cf81c27ca7df1
Farrukh <679@holbertonschool.com>
2019-04-05 23:58:28 +0000

    _strtok, new version

--- main.c
+++ main.c
@@ -5,6 +5,43 @@
 #include <string.h>
 #include <unistd.h>
 
+/**
+ * _strtok - compares two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: returns the differences of ASCII characters
+ */
+
+char *_strtok(char *str, const char *delim)
+{
+	static char *sp;
+	char *p;
+	int letter = 0;
+
+	if (str)
+		sp = str;
+	p = sp;
+	while (sp && *sp)
+	{
+		if (*sp == *delim && *(sp + 1) == *delim)
+		{
+			sp++;
+			continue;
+		}
+		if (*sp == *delim && letter == 1)
+		{
+			*sp = '\0';
+			//sp++;
+		}
+		letter = 1;
+		sp++;
+	}
+	if (!p || *p == '\0')
+		return (NULL);
+	return (p);
+}
+
 
 /**
  * d_space - compares two strings
@@ -44,51 +81,37 @@ char *d_space(char *str)
  * Return: returns the differences of ASCII characters
  */
 
-void tokarr(char *str)
+char  **tokarr(char *str)
 {
 	char *token;
 	char **arr;
-	unsigned int i = 0, count = 1;
-	char *pure = d_space(str);
+	unsigned int i = 0, count = 1, isword = 0, t = 0;
+	char *pure = str;
 
 	while (pure && pure[i])
 	{
-		if (pure[i] == ' ')
+		if (pure[i] != ' ')
+		{
+			isword = 1;
+		}
+		if (isword && pure[i] == ' ')
 			count++;
 		i++;
 	}
-	printf("%d", count);
-}
+	arr = malloc(sizeof(char *) * (count + 1));
 
-/**
- * _strtok - compares two strings
- * @s1: first string
- * @s2: second string
- *
- * Return: returns the differences of ASCII characters
- */
-
-char *_strtok(char *str, const char *delim)
-{
-	static char *sp;
-	char *p;
-
-	if (str)
-		sp = str;
-	p = sp;
-	while (sp && *sp)
+	token = _strtok(str, " ");
+	while (token)
 	{
-		if (*sp == *delim)
-		{
-			*sp++ = '\0';
-			break;
-		}
-		sp++;
+		arr[t] = token;
+		t++;
+		token = _strtok(NULL, " ");
 	}
-	if (!p || *p == '\0')
-		return (NULL);
-	return (p);
+	return (arr);
+
+
 }
+
 
 int main(int argc, char *argv[], char *envp[])
 {
@@ -96,44 +119,49 @@ int main(int argc, char *argv[], char *envp[])
 	size_t len = 0;
 	int get, pid;
 	char *argv1[] = {"", NULL};
-	char str[] = "  Hello World Hi";
-	tokarr(str);
+	char str[] = "Hello    World Hi     ";
+	char **tim = tokarr(str);
+	while (tim[len])
+	{
+		printf("%s\n", tim[len]);
+		len++;
+	}
 	/*while(18)
-		  {
-		  printf("($) ");
-		  get = getline(&buffer, &len, stdin);
-		  buffer[get - 1] = '\0';
-		  pure = d_space(buffer);
-		  //	printf("%s", pure);
-		  if (!strcmp(pure, "exit"))
-		  {
-		  exit(98);
-		  }
-		  if (get == -1)
-		  {
-		  printf("\n");
-		  break;
-		  }
-		  pid = fork();
-		  if (pid < 0)
-		  {
-		  printf("Continue");
-		  continue;
-		  }
-		  if (pid == 0)
-		  {
-		  if (execve(pure, argv1, NULL) == -1)
-		  {
-		  perror("Error:");
-		  return (-1);
-		  }
-		  }
-		  else
-		  {
-		  wait(NULL);
-		  continue;
-		  }
-		  }
-		  free(buffer);*/
+	  {
+		 printf("($) ");
+		 get = getline(&buffer, &len, stdin);
+		 buffer[get - 1] = '\0';
+		 pure = d_space(buffer);
+		 //	printf("%s", pure);
+		 if (!strcmp(pure, "exit"))
+		 {
+			 exit(98);
+		 }
+		 if (get == -1)
+		 {
+			 printf("\n");
+			 break;
+		 }
+		 pid = fork();
+		 if (pid < 0)
+		 {
+			 printf("Continue");
+			 continue;
+		 }
+		 if (pid == 0)
+		 {
+			 if (execve(pure, argv1, NULL) == -1)
+			 {
+				 perror("Error:");
+				 return (-1);
+			 }
+		 }
+		 else
+		 {
+			 wait(NULL);
+			 continue;
+		 }
+		 }
+		 free(buffer);*/
 	return (0);
 }
