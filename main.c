7c2ee3f611da0458f1a49f01d7ee1af3376bbaaa
Farrukh <679@holbertonschool.com>
2019-04-06 20:38:06 +0000

    Resolved merge conflict

--- main.c
+++ main.c
@@ -60,76 +60,8 @@ int main(int argc, char *argv[], char *envp[])
 			wait(NULL);
 			continue;
 		}
-<<<<<<< HEAD
-		if (isword && pure[i] == ' ')
-			count++;
-		i++;
-	}
-	arr = malloc(sizeof(char *) * (count + 1));
-	if (!arr)
-		return (NULL);
-	token = _strtok(str, " ");
-	while (token)
-	{
-		arr[t] = token;
-		t++;
-		token = _strtok(NULL, " ");
-	}
-	arr[t] = NULL;
-	return (arr);
-}
-
-
-int main(int argc, char *argv[], char *envp[])
-{
-	char *buffer, *pure;
-	size_t len = 0, i = 0;
-	int get, pid;
-	char **arr;
-
-	while(18)
-	{
-		printf("($) ");
-		get = getline(&buffer, &len, stdin);
-		buffer[get - 1] = '\0';
-		arr = tokarr(buffer);
-		while (arr[i])
-		{
-			d_space(arr[i]);
-			i++;
-		}
-		if (!strcmp(arr[0], "exit"))
-		{
-			exit(98);
-		}
-		if (get == -1)
-		{
-			printf("\n");
-			break;
-		}
-		pid = fork();
-		if (pid < 0)
-		{
-			printf("Continue");
-			continue;
-		}
-		if (pid == 0)
-		{
-			if (execve(arr[0], arr, NULL) == -1)
-			{
-				perror("Error:");
-				return (-1);
-			}
-		}
-		else
-		{
-			wait(NULL);
-			continue;
-		}
-=======
 		free(arr);
 		free(buffer);
->>>>>>> farrukh
 	}
 	return (0);
 }
