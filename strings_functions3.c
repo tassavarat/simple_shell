d18e1fa06effdc42accc9a9335cadb5b49727e21
Farrukh <679@holbertonschool.com>
2019-04-16 01:41:49 +0000

    Added strings function number 3

---
+++ strings_functions3.c
@@ -0,0 +1,31 @@
+#include "simple_shell.h"
+
+/**
+ * _strdup - duplicates a string
+ * @str: string
+ *
+ * Return: a pointer to the string
+ */
+char *_strdup(const char *str)
+{
+	char *s;
+	int i = 0;
+
+	if (str == NULL)
+		return (NULL);
+	while (str[i])
+	{
+		i++;
+	}
+	s = malloc(sizeof(char) * i + 1);
+	if (s == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i])
+	{
+		s[i] = str[i];
+		i++;
+	}
+	s[i++] = '\0';
+	return (s);
+}
